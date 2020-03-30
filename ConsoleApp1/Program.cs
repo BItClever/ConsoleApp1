05445aff93902a03b55f07035a6971fc3b19b115
Романов Алексей <neonzhlobin@tut.by>
2020-03-30 18:33:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cl = 3;
+            int st = 4;
+            int[,] array = new int[cl, st];
+            Random rand = new Random();
+
+            for (int i = 0; i < cl; i++)
+            {
+                for (int j = 0; j < st; j++)
+                {
+                    array[i, j] = rand.Next(0, 100);
+                    Console.Write(array[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            int[,] tr = new int[st, cl];
+            for (int i = 0; i < st; i++)
+            {
+                for (int j = 0; j < cl; j++)
+                {
+                    tr[i, j] = array[j, i];
+                    Console.Write(tr[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
